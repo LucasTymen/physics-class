160700fec659a6e54c97c3930cfa37edd58f877c
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-15 15:03:12 +0200

    physic-class5

--- script.py
+++ script.py
@@ -7,16 +7,32 @@
 
 # Write your code below:
 
+#step 1
 def f_to_c(f_temp):
   c_temp = (f_temp - 32) * 5/9
   return c_temp
 
+#step 2
 f100_in_celsius = f_to_c(0)
 #print(f100_in_celsius)
 
+#step 3
 def c_to_f(c_temp):
   f_temp = c_temp *(9/5) + 32
   return f_temp
 
+#step 4
 c0_in_fahrenheit = c_to_f(0)
-#print(c0_in_fahrenheit)
+print(c0_in_fahrenheit)
+
+# Step 5
+def get_force(mass, acceleration):
+  return mass * acceleration
+
+#step 6
+#step 7
+#step 8
+#step 9
+#step 11
+#step 12
+#step 13
