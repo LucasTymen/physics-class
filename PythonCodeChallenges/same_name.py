3523b0554c3502764b5383563ec5141c558520c2
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-17 16:03:45 +0200

    exercises

---
+++ PythonCodeChallenges/same_name.py
@@ -0,0 +1,17 @@
+# Write your same_name function here:
+def same_name(your_name, my_name):
+    if str(your_name) == str(my_name): return True
+# Uncomment these function calls to test your same_name function:
+print(same_name("Colby", "Colby"))
+# should print True
+print(same_name("Tina", "Amber"))
+# should print False
+
+"""
+correction code :
+def same_name(your_name, my_name):
+  if (your_name == my_name):
+    return True
+  else:
+    return False
+"""
