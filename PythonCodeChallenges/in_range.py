ce79623bde866c3d4158d780071e842e3df7c089
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-17 15:57:13 +0200

    exercises

---
+++ PythonCodeChallenges/in_range.py
@@ -0,0 +1,23 @@
+# Write your in_range function here:
+def in_range(num,lower, upper):
+    if num >= lower and (num <= upper) :
+        return True
+    return False
+
+
+# Uncomment these function calls to test your in_range function:
+print(in_range(10, 10, 10))
+# should print True
+print(in_range(5, 10, 20))
+# should print False
+
+"""
+# my way :
+def in_range(num,lower, upper):
+    if (num >= lower):
+        return True
+    elif (num >= upper):
+        return True
+    else: return False
+
+"""
