012ec77507cf182d7a18743023a75e05a304ca2e
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-19 10:19:27 +0200

    exercises

--- 5_lists_advanced/2_remove_middle.py
+++ 5_lists_advanced/2_remove_middle.py
@@ -1,7 +1,9 @@
 #Write your function here
+
 def remove_middle(my_list, start, end):
   list(my_list)
   return my_list[:start] + my_list[end+1:]
+
 
 #Uncomment the line below when your function is done
 print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
