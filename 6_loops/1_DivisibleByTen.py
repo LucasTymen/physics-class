3ff4087903c686bb76e2005a43d97dd74b5a7fce
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-20 05:44:19 +0200

    exercises

--- 6_loops/1_DivisibleByTen.py
+++ 6_loops/1_DivisibleByTen.py
@@ -2,9 +2,9 @@
 def divisible_by_ten(nums):
   count = 0
   for number in nums:
-    if number % 10 == 0:
+    if(number % 10 == 0):
       count += 1
-      return nums
+  return count
 
 #Uncomment the line below when your function is done
 print(divisible_by_ten([20, 25, 30, 35, 40]))
