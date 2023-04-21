3fccbdaa329c4c29c0124e3cd7f4ff0f0a851649
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-21 15:16:54 +0200

    exercises

--- 6_loops/4_odd_indices.py
+++ 6_loops/4_odd_indices.py
@@ -1,5 +1,10 @@
 #Write your function here
-
+def odd_indices(my_list):
+    new_list = []
+    for index in range(1, len(my_list), 2):
+       new_list.append(my_list[index])
+    print(new_list)
+    return new_list
 
 #Uncomment the line below when your function is done
-#print(odd_indices([4, 3, 7, 10, 11, -2]))
+print(odd_indices([4, 3, 7, 10, 11, -2]))
