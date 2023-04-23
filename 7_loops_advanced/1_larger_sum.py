ae43c65224d5d318e6406878cdf02d3918e95476
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-23 23:30:50 +0200

    exercises

--- 7_loops_advanced/1_larger_sum.py
+++ 7_loops_advanced/1_larger_sum.py
@@ -6,8 +6,8 @@ def larger_sum(lst1, lst2):
         sum1 += number
     for number in lst2:
         sum2 += number
-    if lst1 < lst2:
-      print(lst1)
-    else: print(lst2)
+    if sum1 > sum2:
+      return lst1
+    else: lst2
 #Uncomment the line below when your function is done
 print(larger_sum([1, 9, 5], [2, 3, 7]))
