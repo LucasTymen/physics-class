da13eca997954355ccef3a350589b61f3b222eea
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-24 22:39:32 +0200

    exercises

---
+++ 9_Functions_advanced/3_Bond_JamesBond.py
@@ -0,0 +1,8 @@
+# Write your introduction function here:
+def introduction(first_name, last_name):
+  return str(last_name)+", "+str(first_name)+" "+str(last_name)
+# Uncomment these function calls to test your introduction function:
+print(introduction("James", "Bond"))
+# should print Bond, James Bond
+print(introduction("Maya", "Angelou"))
+# should print Angelou, Maya Angelou
