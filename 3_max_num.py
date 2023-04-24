9575e438dd7978b9febb1fa456b864c564a54adb
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-24 15:02:23 +0200

    exercises

--- 3_max_num.py
+++ 3_max_num.py
@@ -7,3 +7,14 @@ def max_num(nums):
 
 #Uncomment the line below when your function is done
 print(max_num([50, -10, 0, 75, 20]))
+
+
+################################################
+# alternative solution
+
+def max_num(nums):
+  maximum = nums[0]
+  for number in nums:
+    if number > maximum:
+      maximum = number
+  return maximum
