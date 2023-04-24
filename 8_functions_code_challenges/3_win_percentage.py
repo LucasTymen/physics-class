94844caa3687269d8cfb962ee4834a6b098bdce4
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-24 18:08:36 +0200

    exercises

--- 8_functions_code_challenges/3_win_percentage.py
+++ 8_functions_code_challenges/3_win_percentage.py
@@ -7,3 +7,16 @@ print(win_percentage(5, 5))
 # should print 50
 print(win_percentage(10, 0))
 # should print 100
+
+###################################################
+# the CodeCademy solution :
+def win_percentage(wins, losses):
+  total_games = wins + losses
+  ratio_won = wins / total_games
+  return ratio_won * 100
+
+# Uncomment these function calls to test your win_percentage function:
+print(win_percentage(5, 5))
+# should print 50
+print(win_percentage(10, 0))
+# should print 100
