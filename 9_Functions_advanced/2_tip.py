c168317e15e12044b32f000663d0e7c91c104866
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-24 21:08:54 +0200

    exercises

--- 9_Functions_advanced/2_tip.py
+++ 9_Functions_advanced/2_tip.py
@@ -7,3 +7,16 @@ print(tip(10, 25))
 # should print 2.5
 print(tip(0, 100))
 # should print 0.0
+
+
+#############################################
+# CodeCademy Solution :
+def tip(total, percentage):
+  tip_amount = (total * percentage) / 100
+  return tip_amount
+
+# Uncomment these function calls to test your tip function:
+print(tip(10, 25))
+# should print 2.5
+print(tip(0, 100))
+# should print 0.0
