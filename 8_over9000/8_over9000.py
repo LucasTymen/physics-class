4a54c106166329ba68f55130b3c7442faa2034d6
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-24 12:21:34 +0200

    exercises

--- 8_over9000/8_over9000.py
+++ 8_over9000/8_over9000.py
@@ -1,5 +1,9 @@
 #Write your function here
-
-
+def over_nine_thousand(lst):
+  new_sum = 0
+  for number in lst:
+    number += new_sum
+    return new_sum
+  print(new_sum)
 #Uncomment the line below when your function is done
-#print(over_nine_thousand([8000, 900, 120, 5000]))
+print(over_nine_thousand([8000, 900, 120, 5000]))
